2bb6bb8b9b6c4c229323ce66fd87c642593e3a57
RyanRMurray@Hotmail.co.uk <RyanRMurray@Hotmail.co.uk>
2019-10-13 14:50:08 +0100

    finished xml input, started on image output

--- src/TileGen.hs
+++ src/TileGen.hs
@@ -1,19 +1,107 @@
 module TileGen where
-    import Text.XML as XML
-    import Text.XML.Lens
+  import Util (readRational)
+  import Data.List
+  import Data.Maybe
+  import qualified Data.Map as Map
+  import Data.Map (Map)
+  import Text.XML.Light
+  import Data.List.Split
+  import Codec.Picture
+  import Control.Monad
 
-    data SymType = X | T | I | L | O deriving (Show)    
-    data Tile = Tile 
-        { tileName  :: String
-        , symmetry  :: SymType
-        , weighting :: Rational
-        } deriving (Show)
+  data Rot = N | W | S | E deriving (Show)
+  data Tile = Tile 
+      { tileName  :: String
+      , symmetry  :: String
+      , weighting :: Rational
+      } deriving (Show)
 
-    
---functions for parsing XML for tiles
------------------------------------------------------------------------------------------------------------------
+  data TileConfiguration = TileConfiguration Integer Rot deriving (Show)
+  data Neighbor = Neighbour
+      { left     :: TileConfiguration
+      , right    :: TileConfiguration
+      } deriving (Show)
 
---parseXML :: String -> Either ? [String]
---parseXML fpath = do
-  --  Document _ root _ <- readFile def fpath
-    
+  data TileData = TileData
+      { tiles :: [Tile]
+      , validNeighbours :: [Neighbor]
+      , images :: [DynamicImage]
+      }
+  
+  type CoOrd = (Integer, Integer)
+  type OutGrid = (Map CoOrd TileConfiguration)
+
+--Functions for importing tile data from XML files
+--Functions assume a correctly-formatted input
+---------------------------------------------------------------------------------------------------
+  --return Just the requested element
+  findEl :: String -> Element -> Element
+  findEl name parent = fromJust $ findElement (unqual name) parent
+  
+  --return Just the requested element. Since weightings can be blank, return default of 0 for them.
+  justAttr :: String -> Element -> String
+  justAttr a e = 
+    case findAttr (unqual a) e of
+      Just x -> x
+      _      -> "0.0"
+
+  --get tiles and neoighbours data
+  getTileData :: FilePath -> IO (Either String TileData)
+  getTileData fpath = do
+    xml <- parseXMLDoc <$> readFile (fpath ++ "data.xml")
+    --get tiles
+    let xTiles = elChildren $ findEl "tiles" $ findEl "set" $ fromJust xml
+    let tnames = map (justAttr "name") xTiles
+    let tsyms  = map (justAttr "symmetry") xTiles
+    let tweis  = map (justAttr "weight") xTiles
+    let tData  = zip3 tnames tsyms tweis
+    let tiles  = map mkTile tData
+    --get neighbours
+    let xNs    = elChildren $ findEl "neighbors" $ findEl "set" $ fromJust xml
+    let ln     = map (justAttr "left") xNs
+    let rn     = map (justAttr "right") xNs
+    let nData  = zip ln rn
+    let neighbors = map (mkNeighbors tnames) nData
+    --get images
+    images <- sequence <$> mapM (\n -> readImage $ fpath ++ n ++ ".png") tnames 
+    case images of
+      Left err -> return $ Left err
+      Right is -> return $ Right $ TileData tiles neighbors is
+
+  --make tile from truple
+  mkTile :: (String, String, String) -> Tile
+  mkTile (name, sym, weight) = Tile name sym (readRational weight)
+
+  --make a valid neighbour pair from the name of two tiles
+  mkNeighbors :: [String] -> (String, String) -> Neighbor
+  mkNeighbors ts (l, r) = Neighbour (mkConfig ts (splitOn " " l)) (mkConfig ts  (splitOn " " r))
+
+  --make a TileConfiguration from the name of the tile and an integer representing its rotation
+  mkConfig :: [String] -> [String] -> TileConfiguration
+  mkConfig ts [id]      = TileConfiguration (mkConfig' id ts) N
+  mkConfig ts [id, "1"] = TileConfiguration (mkConfig' id ts) W
+  mkConfig ts [id, "2"] = TileConfiguration (mkConfig' id ts) S
+  mkConfig ts [id, "3"] = TileConfiguration (mkConfig' id ts) E
+
+  mkConfig' id ts = toInteger $ fromJust $ elemIndex id ts
+
+  --Functions for printing
+  -------------------------------------------------------------------------------------------------
+  
+  gridX = 1
+  gridY = 1
+
+  testgrid = Map.fromList [((0,0), TileConfiguration 0 N),((0,1), TileConfiguration 1 N),((1,0), TileConfiguration 1 N),((1,1), TileConfiguration 0 N)]
+
+  --extract the image IDs for each panel in the grid, with -1 defaults
+  getImageIDGrid :: OutGrid -> [[Integer]]
+  getImageIDGrid grid = [getImageIDLine grid y | y <- [0..gridY]]
+
+  getImageIDLine :: OutGrid -> Integer -> [Integer]
+  getImageIDLine grid y = [getImageID $ getTile grid (x,y) | x <- [0..gridX]]
+
+  getTile :: OutGrid -> CoOrd -> TileConfiguration
+  getTile grid coord = Map.findWithDefault (TileConfiguration (-1) N) coord grid
+  
+  getImageID :: TileConfiguration -> Integer
+  getImageID (TileConfiguration i _) = i
